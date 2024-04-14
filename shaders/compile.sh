63c5b2ab15c3811d1019c48a3b9ac0ca745c94af
TommyGio3 <tommaso.giorgeschi@mail.polimi.it>
2024-04-14 22:16:34 +0200

    Added files

---
+++ shaders/compile.sh
@@ -0,0 +1,3 @@
+/Users/tommasogiorgeschi/VulkanSDK/1.3.204.1/macOS/bin/glslc Shader.vert -o vert.spv
+/Users/tommasogiorgeschi/VulkanSDK/1.3.204.1/macOS/bin/glslc Shader.frag -o frag.spv
+/Users/tommasogiorgeschi/VulkanSDK/1.3.204.1/macOS/bin/glslc shader_boat.frag -o frag_boat.spv
